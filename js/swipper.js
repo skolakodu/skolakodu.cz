b2d10f1af0cb4b4829b6db06dc15b9a0fcddfdbe
Roman Amelin <romanamelin@Romans-MBP.lan>
2022-09-21 17:25:12 +0200

    updates

--- js/swipper.js
+++ js/swipper.js
@@ -1,10 +1,43 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper1', {
     // Optional parameters
     direction: 'horizontal',
     slidesPerView: 5,
     spaceBetween: 20,
     navigation: false,
-    
-  });
+    slidesOffsetBefore: 200,
+    slidesOffsetAfter: 50,
+    // breakpoints: {
+    //   // when window width is >= 320px
+    //   320: {
+    //     slidesPerView: 1,
+    //     slidesOffsetBefore: 20,
+    //   },
+    //   // when window width is >= 480px
+    //   480: {
+    //     slidesPerView: 3,
+    //     slidesOffsetBefore: 20
+    //   },
+    //   // when window width is >= 640px
+    //   640: {
+    //     slidesPerView: 4,
+    //     slidesOffsetBefore: 20
+    //   }
+    // }
+});
 
 
+const swiperReviews = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: true,
+});
